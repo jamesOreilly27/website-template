eca7e4382b64f907101e34572d94157c976e5277
James O'Reilly <James.Oreilly926@gmail.com>
2019-04-30 12:49:04 -0400

    add modules that I will use to make https server

--- server/app.js
+++ server/app.js
@@ -4,7 +4,10 @@ const volleyball = require('volleyball')
 const bodyParser = require('body-parser')
 const chalk = require('chalk')
 const path = require('path'); 
-const PORT = 8332
+const PORT = process.env.PORT || 8332
+const https = require('https')
+const fs = reqire('fs')
+const dir = require('os').homedir()
 
 app.use(volleyball)
 app.use(bodyParser.json())
@@ -13,4 +16,4 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use('*', (req, res, next) => res.sendFile(path.join(__dirname, '..', 'public', 'index.html')))
-app.listen(PORT, () => console.log(chalk.blue.bgWhite.bold(`We are live on port ${PORT}`))) 
+app.listen(PORT, () => console.log(chalk.blue.bgWhite.bold(`We are live on port ${PORT}`)))
